8ff9c86802513a07b97d05075c9d543dd49d945f
anton.bondarev1978@gmail.com <anton.bondarev1978@gmail.com@41ba89fb-db7e-818a-ac9e-0c00513835f7>
2011-05-09 12:49:42 +0000

    rename

--- trunk/robobot-android/src/org/embox/robobot/DeviceListActivity.java
+++ trunk/robobot-android/src/org/embox/robobot/DeviceListActivity.java
@@ -18,8 +18,6 @@ package org.embox.robobot;
 
 import java.util.Set;
 
-import com.example.android.BluetoothChat.R;
-
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -30,14 +28,14 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.Window;
 import android.view.View.OnClickListener;
+import android.view.Window;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.AdapterView.OnItemClickListener;
 
 /**
  * This Activity appears as a dialog. It lists any paired devices and
