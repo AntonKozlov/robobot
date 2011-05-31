cbcc76d7f7878441d878b861597ea41ccda8e175
drakon.mega@gmail.com <drakon.mega@gmail.com@41ba89fb-db7e-818a-ac9e-0c00513835f7>
2011-05-31 19:41:08 +0000

    found bt dev list

---
+++ trunk/robobot-android-2/src/org/embox/robobot/transport/BluetoothTransportSocket.java
@@ -0,0 +1,31 @@
+package org.embox.robobot.transport;
+
+import org.embox.robobot.IDevice;
+
+public class BluetoothTransportSocket implements ITransportSocket {
+
+	@Override
+	public byte[] read(int size) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void write(byte[] msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void close() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public IDevice getDevice() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
