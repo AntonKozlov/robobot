5e2c2dc1ab2f50b33fc24f77b0bc49c7f5012f73
Eldar Abusalimov <abusalimov@users.noreply.github.com>
2012-07-07 13:06:12 +0000

    use logging instead of 'print >>stderr'

--- robobot-sim/main.py
+++ robobot-sim/main.py
@@ -1,41 +1,51 @@
 #!/usr/bin/env python2
 
-import socket
 import sys
+import socket
+import logging
+
+root_logger = logging.getLogger()
+root_logger.setLevel(logging.DEBUG)
+root_logger.addHandler(logging.StreamHandler(sys.stderr))
+
+log = logging.getLogger(__name__)
 
 def echo_server(port):
     # Create a TCP/IP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Bind the socket to the port
-    server_address = ('localhost', port)
-    print >>sys.stderr, 'starting up on %s port %s' % server_address
-    sock.bind(server_address)
+    try:
+        # Bind the socket to the port
+        server_address = ('localhost', port)
+        log.info('starting up on %s port %s', *server_address)
+        sock.bind(server_address)
 
-    # Listen for incoming connections
-    sock.listen(1)
+        # Listen for incoming connections
+        sock.listen(1)
 
-    while True: # Wait for a connection
-        print >>sys.stderr, 'waiting for a connection'
-        connection, client_address = sock.accept()
-        try:
-            print >>sys.stderr, 'connection from', client_address
+        while True: # Wait for a connection
+            log.info('waiting for a connection')
+            connection, client_address = sock.accept()
+            try:
+                log.info('connection from %s', client_address)
 
-            # Receive the data in small chunks and retransmit it
-            while True:
-                data = connection.recv(16)
-                print >>sys.stderr, 'received "%s"' % data
-                if data:
-                    print >>sys.stderr, 'sending data back to the client'
-                    connection.sendall(data)
-                else:
-                    print >>sys.stderr, 'no more data from', client_address
-                    break
-                
-        finally:
-            # Clean up the connection
-            connection.close()
+                # Receive the data in small chunks and retransmit it
+                while True:
+                    data = connection.recv(16)
+                    log.info('received "%s"', data)
+                    if data:
+                        log.info('sending data back to the client')
+                        connection.sendall(data)
+                    else:
+                        log.info('no more data from %s', client_address)
+                        break
+
+            finally:
+                # Clean up the connection
+                connection.close()
+    finally:
+        sock.close()
 
 if __name__ == '__main__':
-    echo_server(10000) 
+    echo_server(10000)
 
