e2c9529bae282a35208e1e555a85563b0e88ff8b
Vita Loginova <vita.loginova@gmail.com>
2012-07-10 13:42:37 +0000

    Another way sensor handling

---
+++ robobot-winphone/robobot-winphone/Model/ISensorView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace robobot_winphone.Model
+{
+    public interface ISensorView
+    {
+        void ProcessSensorData(int turn, int speed);
+    }
+}
---
+++ robobot-winphone/robobot-winphone/Model/SensorHandler/ACGSensorHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Microsoft.Xna.Framework;
+using System.Text;
+using Microsoft.Devices.Sensors;
+using robobot_winphone.ViewModel;
+
+namespace robobot_winphone.Model.SensorHandler
+{
+    public class ACGSensorHandler
+    {
+        
+    }
+}
