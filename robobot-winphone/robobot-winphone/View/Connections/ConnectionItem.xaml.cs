ec038fc3e16dbbaca6a1de3c7790aaa34af0ad53
Vita Loginova <vita.loginova@gmail.com>
2012-09-13 14:42:34 +0000

    connection list context menu and choosing by it added

--- robobot-winphone/robobot-winphone/View/Connections/ConnectionItem.xaml.cs
+++ robobot-winphone/robobot-winphone/View/Connections/ConnectionItem.xaml.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
 
 namespace robobot_winphone.View.Connections
 {
@@ -17,6 +10,27 @@ namespace robobot_winphone.View.Connections
         public ConnectionItem()
         {
             InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            FindParentOfType<PhoneApplicationPage>(this).NavigationService.GoBack();
+        }
+
+        public static T FindParentOfType<T>(FrameworkElement element)
+        {
+            var parent = VisualTreeHelper.GetParent(element) as FrameworkElement;
+
+
+            while (parent != null)
+            {
+                if (parent is T)
+                    return (T)(object)parent;
+
+
+                parent = VisualTreeHelper.GetParent(parent) as FrameworkElement;
+            }
+            return default(T);
         }
     }
 }
